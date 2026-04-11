aea90b56fe669f069864fa4e76593af213152658
xTymezz <tymezzmasseywelsh1@gmail.com>
2026-04-11 21:29:52 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -3,4 +3,4 @@ local part = Instance.new('Part',workspace)
 part.Anchored = true
 part.Size = Vector3.new(4,2,4)
 part.Position = workspace.Baseplate.Position + Vector3.new(0,10,0)
-ReplicatedStorage:WaitForChild('Animate'):Fire(5, part, {Transparency = 1}, 1)
+ReplicatedStorage:WaitForChild('Animate'):Fire(true, part, {Transparency = 1}, 1)
